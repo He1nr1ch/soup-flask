f397aeceb2406d7d1162886a7cb3265d2f6e247f
Heinrich Beck <heinrich@iheinrich.com>
2021-06-06 21:33:36 +0500

    created writing app

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask import render_template
+import steam as status
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
+    status.Misha()
     return render_template("default.html")
--- steam.py
+++ steam.py
@@ -1,13 +1,29 @@
+from datetime import datetime
 from bs4 import BeautifulSoup
-
 import requests
 
+def Misha():
+    website = requests.get('https://steamcommunity.com/id/BuriyDude/')
+    soup = BeautifulSoup(website.content, 'lxml')
 
-website = requests.get('https://steamcommunity.com/id/BuriyDude/')
-soup = BeautifulSoup(website.content, 'lxml')
+    data = soup.find('div', class_ = 'profile_in_game_header')
+    game = soup.find('div', class_ = 'profile_in_game_name')
 
-data = soup.find('div', class_ = 'profile_in_game_header')
-game = soup.find('div', class_ = 'profile_in_game_name')
+    print(data.text)
 
-print(data.text)
-print(game.text)
+    if game == None:
+        game_status = "<a> Not playing </a>"
+    else:
+        game_status = '<a>' + game.text + '</a>'
+
+    now = datetime.now() # current date and time
+
+    bettertime = now.strftime('%Y-%m-%d %H-%M')
+
+
+    print(bettertime)
+
+    f = open("templates/record.html", "a")
+    f.write('<a>' + bettertime + ' </a>' + ' <a> ' + data.text + ' </a> ' + game_status + '<br>')
+    f.close()
+
